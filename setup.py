90167c0cd7518337b50e14142ee02731071c9dc2
Tom van Dijk <tom@tvandijk.nl>
2015-02-16 01:59:10 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hemres',
+    version='0.1',
+    packages=['hemres', 'hemres.migrations', 'hemres.templatetags'],
+    url='http://github.com/jonge-democraten/hemres/',
+    author='Jonge Democraten',
+    author_email='ict@jongedemocraten.nl',
+    description='Webapp for sending newsletters',
+    include_package_data=True,
+    license='MIT',
+)
