6480826f0d52c794ce71372a699713dcf4310eb6
Tom van Dijk <tom@tvandijk.nl>
2015-04-26 02:58:29 +0200

    Add django-rq to 'nieuws' urls

--- nieuws/urls.py
+++ nieuws/urls.py
@@ -4,5 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^django-rq/', include('django_rq.urls')),
     url(r'', include('hemres.urls')),
 )
