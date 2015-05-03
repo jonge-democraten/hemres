03f04211a8d75aded4723f8c75f294665cb57e58
Tom van Dijk <tom@tvandijk.nl>
2015-05-03 23:59:09 +0200

    0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hemres',
-    version='0.1',
+    version='0.2',
     packages=['hemres', 'hemres.migrations', 'hemres.templatetags'],
     url='http://github.com/jonge-democraten/hemres/',
     author='Jonge Democraten',
