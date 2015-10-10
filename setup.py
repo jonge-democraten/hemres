dfbc05e3021012d521dfcc9495b3a92b3d81a46d
Tom van Dijk <tom@tvandijk.nl>
2015-10-10 15:09:31 +0200

    Some improvements on the requirements.

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author_email='ict@jongedemocraten.nl',
     description='Webapp for sending newsletters',
     include_package_data=True,
-    install_requires=['html2text>=2015.4.14', 'bleach>=1.4.1'],
+    install_requires=['html2text>=2015.6.21', 'bleach>=1.4.2', 'django-rq>=0.8.0', 'inlinestyler>=0.2.1', 'future>=0.15.2'],
     license='MIT',
 )
