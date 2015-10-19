733d2c7ee58a04c72e3ec8724605de5477e62979
Tom van Dijk <tom@tvandijk.nl>
2015-10-20 01:22:35 +0200

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='hemres',
-    version='0.9',
+    version='0.10',
     packages=['hemres', 'hemres.migrations', 'hemres.templatetags'],
     url='http://github.com/jonge-democraten/hemres/',
     author='Jonge Democraten',
