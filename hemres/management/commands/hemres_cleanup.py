94a9377a37c8f5431f63acd8cb72269959f8a04f
Tom van Dijk <tom@tvandijk.nl>
2017-06-11 12:18:14 +0200

    Now also remove email subscribers without subscription when cleanup is called

--- hemres/management/commands/hemres_cleanup.py
+++ hemres/management/commands/hemres_cleanup.py
@@ -1,14 +1,18 @@
 from __future__ import print_function
 from django.conf import settings
 from django.core.management.base import BaseCommand
-from hemres.models import JaneusSubscriber
+from django.db import models
+from hemres.models import JaneusSubscriber, EmailSubscriber
 from janeus import Janeus
 
 
 class Command(BaseCommand):
-    help = 'Delete the subscriptions of Janeus users that no longer exist'
+    help = 'Delete the subscriptions of Janeus users that no longer exist and of email subscribers without subscriptions'
 
     def handle(self, *args, **kwargs):
+        for s in EmailSubscriber.objects.annotate(num=models.Count('subscriptions')).filter(num=0):
+            s.delete()
+
         if not hasattr(settings, 'JANEUS_SERVER'):
             print("Janeus is not configured!")
             return
