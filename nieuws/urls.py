d90765d4d233123b4031114ef17d2125a33b626c
Tom van Dijk <tom@tvandijk.nl>
2017-10-22 12:08:38 +0200

    Remove redis code, newsletters are no longer sent!

--- nieuws/urls.py
+++ nieuws/urls.py
@@ -4,6 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns(
     '',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^django-rq/', include('django_rq.urls')),
     url(r'', include('hemres.urls')),
 )
