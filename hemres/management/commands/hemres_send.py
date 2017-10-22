53cd7d286c687ebdbe6579070f71f29eae6abbe2
Tom van Dijk <tom@tvandijk.nl>
2017-10-22 13:15:20 +0200

    Now use a management command for sending emails, must be cronjobbed!

---
+++ hemres/management/commands/hemres_send.py
@@ -0,0 +1,57 @@
+from django.core.management.base import BaseCommand
+from hemres import models
+from filelock import FileLock, Timeout
+import logging
+import time
+
+
+logger = logging.getLogger(__name__)
+if not logger.handlers:
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(fmt='%(asctime)s %(message)s', datefmt='%H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+class Command(BaseCommand):
+    help = 'Start the Hemres worker'
+
+    def add_arguments(self, parser):
+        parser.add_argument('lock_filename', help="filename of lockfile used to ensure only one send is running")
+
+    def send_mails(self):
+        try:
+            # Get all "tasks" from the database
+            for ns in models.NewsletterToSubscriber.objects.all():
+                # Check if the email address is not empty
+                if ns.target_email == "":
+                    logger.info("Skipping and deleting {} due to no email address".format(ns.pk))
+                    ns.delete()
+                elif ns.pk in self.failing_pks:
+                    # Do not try sending if we had an exception last time we tried...
+                    pass
+                else:
+                    logger.info("Sending {}...".format(ns.pk))
+                    try:
+                        # Try sending the email, and report exception if it didn't work
+                        # Note that send_mail also deletes the object from the database...
+                        # Note that Django turns "autocommit" on for database transactions,
+                        # this is important as otherwise our DELETE queries are never committed.
+                        ns.send_mail()
+                    except Exception as e:
+                        logger.exception(e)
+                        self.failing_pks.append(ns.pk)
+        except Exception as e:
+            logger.exception(e)
+
+    def handle(self, *args, **kwargs):
+        # We require 1 additional argument, which is the filename for locking
+        try:
+            with FileLock(kwargs['lock_filename'], timeout=0):
+                logger.info("Going to send mails")
+                self.failing_pks = []
+                self.send_mails()
+                logger.info("Done, bye!")
+        except Timeout:
+            logger.info("Not sending mails, lockfile locked")
